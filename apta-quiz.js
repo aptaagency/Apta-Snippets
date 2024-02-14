4802b3ac5cc7c721a6b4c7d64988b88fc427ea8f
Maria Clara <142933377+MariaClaraApta@users.noreply.github.com>
2024-02-14 10:04:23 -0300

    update apta-quiz.js

--- apta-quiz.js
+++ apta-quiz.js
@@ -2,82 +2,781 @@ const options = [
   {
     destination: "30A, Florida",
     fields: [
-      { option: "A", value: 10 },
-      { option: "B", value: 2 },
-      { option: "C", value: 3 },
-      { option: "D", value: 5 },
+      {
+        questions: "q1",
+        values: [
+          {
+            option: "A",
+            value: 10,
+          },
+          {
+            option: "B",
+            value: 2,
+          },
+          {
+            option: "C",
+            value: 3,
+          },
+          {
+            option: "D",
+            value: 5,
+          },
+        ],
+      },
+      {
+        questions: "Q2",
+        values: [
+          {
+            option: "A",
+            value: 8,
+          },
+          {
+            option: "B",
+            value: 4,
+          },
+          {
+            option: "C",
+            value: 7,
+          },
+          {
+            option: "D",
+            value: 2,
+          },
+        ],
+      },
+      {
+        questions: "Q4",
+        values: [
+          {
+            option: "A",
+            value: 6,
+          },
+          {
+            option: "B",
+            value: 10,
+          },
+          {
+            option: "C",
+            value: 4,
+          },
+          {
+            option: "D",
+            value: 8,
+          },
+        ],
+      },
+      {
+        questions: "Q5",
+        values: [
+          {
+            option: "A",
+            value: 10,
+          },
+          {
+            option: "B",
+            value: 6,
+          },
+          {
+            option: "C",
+            value: 8,
+          },
+          {
+            option: "D",
+            value: 7,
+          },
+        ],
+      },
+      {
+        questions: "Q7",
+        values: [
+          {
+            option: "A",
+            value: 7,
+          },
+          {
+            option: "B",
+            value: 8,
+          },
+          {
+            option: "C",
+            value: 6,
+          },
+          {
+            option: "D",
+            value: 9,
+          },
+        ],
+      },
+      {
+        questions: "Q8",
+        values: [
+          {
+            option: "A",
+            value: 8,
+          },
+          {
+            option: "B",
+            value: 6,
+          },
+          {
+            option: "C",
+            value: 9,
+          },
+          {
+            option: "D",
+            value: 5,
+          },
+        ],
+      },
+      {
+        questions: "Q10",
+        values: [
+          {
+            option: "A",
+            value: 7,
+          },
+          {
+            option: "B",
+            value: 9,
+          },
+          {
+            option: "C",
+            value: 8,
+          },
+          {
+            option: "D",
+            value: 10,
+          },
+        ],
+      },
     ],
   },
   {
     destination: "Abaco, Bahamas",
     fields: [
-      { option: "A", value: 10 },
-      { option: "B", value: 3 },
-      { option: "C", value: 2 },
-      { option: "D", value: 6 },
+      {
+        questions: "Q1",
+        values: [
+          {
+            option: "A",
+            value: 10,
+          },
+          {
+            option: "B",
+            value: 3,
+          },
+          {
+            option: "C",
+            value: 2,
+          },
+          {
+            option: "D",
+            value: 6,
+          },
+        ],
+      },
+      {
+        questions: "Q2",
+        values: [
+          {
+            option: "A",
+            value: 10,
+          },
+          {
+            option: "B",
+            value: 5,
+          },
+          {
+            option: "C",
+            value: 4,
+          },
+          {
+            option: "D",
+            value: 3,
+          },
+        ],
+      },
+      {
+        questions: "Q3",
+        values: [
+          {
+            option: "A",
+            value: 7,
+          },
+          {
+            option: "B",
+            value: 9,
+          },
+          {
+            option: "C",
+            value: 5,
+          },
+          {
+            option: "D",
+            value: 3,
+          },
+        ],
+      },
+      {
+        questions: "Q4",
+        values: [
+          {
+            option: "A",
+            value: 7,
+          },
+          {
+            option: "B",
+            value: 9,
+          },
+          {
+            option: "C",
+            value: 5,
+          },
+          {
+            option: "D",
+            value: 6,
+          },
+        ],
+      },
+      {
+        questions: "Q5",
+        values: [
+          {
+            option: "A",
+            value: 8,
+          },
+          {
+            option: "B",
+            value: 7,
+          },
+          {
+            option: "C",
+            value: 9,
+          },
+          {
+            option: "D",
+            value: 10,
+          },
+        ],
+      },
+      {
+        questions: "Q6",
+        values: [
+          {
+            option: "A",
+            value: 9,
+          },
+          {
+            option: "B",
+            value: 7,
+          },
+          {
+            option: "C",
+            value: 8,
+          },
+          {
+            option: "D",
+            value: 5,
+          },
+        ],
+      },
+      {
+        questions: "Q7",
+        values: [
+          {
+            option: "A",
+            value: 6,
+          },
+          {
+            option: "B",
+            value: 8,
+          },
+          {
+            option: "C",
+            value: 7,
+          },
+          {
+            option: "D",
+            value: 9,
+          },
+        ],
+      },
     ],
   },
   {
     destination: "Deer Valley, UT",
     fields: [
-      { option: "A", value: 4 },
-      { option: "B", value: 10 },
-      { option: "C", value: 3 },
-      { option: "D", value: 6 },
+      {
+        questions: "Q1",
+        values: [
+          { option: "A", value: 4 },
+          { option: "B", value: 10 },
+          { option: "C", value: 3 },
+          { option: "D", value: 6 },
+        ],
+      },
+      {
+        questions: "Q2",
+        values: [
+          { option: "A", value: 3 },
+          { option: "B", value: 7 },
+          { option: "C", value: 5 },
+          { option: "D", value: 10 },
+        ],
+      },
+      {
+        questions: "Q4",
+        values: [
+          { option: "A", value: 5 },
+          { option: "B", value: 7 },
+          { option: "C", value: 9 },
+          { option: "D", value: 8 },
+        ],
+      },
+      {
+        questions: "Q5",
+        values: [
+          { option: "A", value: 7 },
+          { option: "B", value: 10 },
+          { option: "C", value: 6 },
+          { option: "D", value: 8 },
+        ],
+      },
+      {
+        questions: "Q7",
+        values: [
+          { option: "A", value: 4 },
+          { option: "B", value: 8 },
+          { option: "C", value: 10 },
+          { option: "D", value: 7 },
+        ],
+      },
+      {
+        questions: "Q8",
+        values: [
+          { option: "A", value: 7 },
+          { option: "B", value: 8 },
+          { option: "C", value: 9 },
+          { option: "D", value: 6 },
+        ],
+      },
+      {
+        questions: "Q10",
+        values: [
+          { option: "A", value: 6 },
+          { option: "B", value: 8 },
+          { option: "C", value: 10 },
+          { option: "D", value: 9 },
+        ],
+      },
     ],
   },
   {
     destination: "Los Cabos, Mexico",
     fields: [
-      { option: "A", value: 10 },
-      { option: "B", value: 3 },
-      { option: "C", value: 8 },
-      { option: "D", value: 6 },
+      {
+        questions: "Q1",
+        values: [
+          { option: "A", value: 10 },
+          { option: "B", value: 3 },
+          { option: "C", value: 8 },
+          { option: "D", value: 6 },
+        ],
+      },
+      {
+        questions: "Q2",
+        values: [
+          { option: "A", value: 9 },
+          { option: "B", value: 6 },
+          { option: "C", value: 5 },
+          { option: "D", value: 4 },
+        ],
+      },
+      {
+        questions: "Q4",
+        values: [
+          { option: "A", value: 7 },
+          { option: "B", value: 8 },
+          { option: "C", value: 10 },
+          { option: "D", value: 6 },
+        ],
+      },
+      {
+        questions: "Q5",
+        values: [
+          { option: "A", value: 9 },
+          { option: "B", value: 7 },
+          { option: "C", value: 6 },
+          { option: "D", value: 8 },
+        ],
+      },
+      {
+        questions: "Q7",
+        values: [
+          { option: "A", value: 8 },
+          { option: "B", value: 7 },
+          { option: "C", value: 6 },
+          { option: "D", value: 9 },
+        ],
+      },
+      {
+        questions: "Q8",
+        values: [
+          { option: "A", value: 6 },
+          { option: "B", value: 10 },
+          { option: "C", value: 8 },
+          { option: "D", value: 5 },
+        ],
+      },
+      {
+        questions: "Q10",
+        values: [
+          { option: "A", value: 7 },
+          { option: "B", value: 8 },
+          { option: "C", value: 9 },
+          { option: "D", value: 10 },
+        ],
+      },
     ],
   },
   {
     destination: "Maui, HI",
     fields: [
-      { option: "A", value: 10 },
-      { option: "B", value: 5 },
-      { option: "C", value: 2 },
-      { option: "D", value: 7 },
+      {
+        questions: "Q1",
+        values: [
+          { option: "A", value: 10 },
+          { option: "B", value: 5 },
+          { option: "C", value: 2 },
+          { option: "D", value: 7 },
+        ],
+      },
+      {
+        questions: "Q2",
+        values: [
+          { option: "A", value: 10 },
+          { option: "B", value: 7 },
+          { option: "C", value: 4 },
+          { option: "D", value: 8 },
+        ],
+      },
+      {
+        questions: "Q4",
+        values: [
+          { option: "A", value: 9 },
+          { option: "B", value: 7 },
+          { option: "C", value: 8 },
+          { option: "D", value: 6 },
+        ],
+      },
+      {
+        questions: "Q5",
+        values: [
+          { option: "A", value: 8 },
+          { option: "B", value: 6 },
+          { option: "C", value: 7 },
+          { option: "D", value: 10 },
+        ],
+      },
+      {
+        questions: "Q7",
+        values: [
+          { option: "A", value: 6 },
+          { option: "B", value: 8 },
+          { option: "C", value: 9 },
+          { option: "D", value: 10 },
+        ],
+      },
+      {
+        questions: "Q8",
+        values: [
+          { option: "A", value: 7 },
+          { option: "B", value: 8 },
+          { option: "C", value: 6 },
+          { option: "D", value: 5 },
+        ],
+      },
+      {
+        questions: "Q10",
+        values: [
+          { option: "A", value: 8 },
+          { option: "B", value: 10 },
+          { option: "C", value: 7 },
+          { option: "D", value: 9 },
+        ],
+      },
     ],
   },
   {
     destination: "Palm Springs, CA",
     fields: [
-      { option: "A", value: 5 },
-      { option: "B", value: 4 },
-      { option: "C", value: 10 },
-      { option: "D", value: 6 },
+      {
+        questions: "Q1",
+        values: [
+          { option: "A", value: 5 },
+          { option: "B", value: 4 },
+          { option: "C", value: 10 },
+          { option: "D", value: 6 },
+        ],
+      },
+      {
+        questions: "Q2",
+        values: [
+          { option: "A", value: 6 },
+          { option: "B", value: 5 },
+          { option: "C", value: 9 },
+          { option: "D", value: 3 },
+        ],
+      },
+      {
+        questions: "Q4",
+        values: [
+          { option: "A", value: 7 },
+          { option: "B", value: 9 },
+          { option: "C", value: 8 },
+          { option: "D", value: 5 },
+        ],
+      },
+      {
+        questions: "Q5",
+        values: [
+          { option: "A", value: 9 },
+          { option: "B", value: 8 },
+          { option: "C", value: 7 },
+          { option: "D", value: 10 },
+        ],
+      },
+      {
+        questions: "Q7",
+        values: [
+          { option: "A", value: 5 },
+          { option: "B", value: 9 },
+          { option: "C", value: 7 },
+          { option: "D", value: 8 },
+        ],
+      },
+      {
+        questions: "Q8",
+        values: [
+          { option: "A", value: 6 },
+          { option: "B", value: 8 },
+          { option: "C", value: 10 },
+          { option: "D", value: 7 },
+        ],
+      },
+      {
+        questions: "Q10",
+        values: [
+          { option: "A", value: 7 },
+          { option: "B", value: 8 },
+          { option: "C", value: 9 },
+          { option: "D", value: 10 },
+        ],
+      },
     ],
   },
   {
     destination: "Scottsdale, AZ",
     fields: [
-      { option: "A", value: 4 },
-      { option: "B", value: 5 },
-      { option: "C", value: 10 },
-      { option: "D", value: 7 },
+      {
+        questions: "Q1",
+        values: [
+          { option: "A", value: 4 },
+          { option: "B", value: 5 },
+          { option: "C", value: 10 },
+          { option: "D", value: 7 },
+        ],
+      },
+      {
+        questions: "Q2",
+        values: [
+          { option: "A", value: 5 },
+          { option: "B", value: 7 },
+          { option: "C", value: 8 },
+          { option: "D", value: 4 },
+        ],
+      },
+      {
+        questions: "Q4",
+        values: [
+          { option: "A", value: 8 },
+          { option: "B", value: 9 },
+          { option: "C", value: 7 },
+          { option: "D", value: 6 },
+        ],
+      },
+      {
+        questions: "Q5",
+        values: [
+          { option: "A", value: 10 },
+          { option: "B", value: 7 },
+          { option: "C", value: 8 },
+          { option: "D", value: 9 },
+        ],
+      },
+      {
+        questions: "Q7",
+        values: [
+          { option: "A", value: 6 },
+          { option: "B", value: 8 },
+          { option: "C", value: 7 },
+          { option: "D", value: 9 },
+        ],
+      },
+      {
+        questions: "Q8",
+        values: [
+          { option: "A", value: 7 },
+          { option: "B", value: 9 },
+          { option: "C", value: 10 },
+          { option: "D", value: 6 },
+        ],
+      },
+      {
+        questions: "Q10",
+        values: [
+          { option: "A", value: 8 },
+          { option: "B", value: 9 },
+          { option: "C", value: 7 },
+          { option: "D", value: 10 },
+        ],
+      },
     ],
   },
   {
     destination: "Tamarindo, Costa Rica",
     fields: [
-      { option: "A", value: 10 },
-      { option: "B", value: 6 },
-      { option: "C", value: 3 },
-      { option: "D", value: 8 },
+      {
+        questions: "Q1",
+        values: [
+          { option: "A", value: 10 },
+          { option: "B", value: 6 },
+          { option: "C", value: 3 },
+          { option: "D", value: 8 },
+        ],
+      },
+      {
+        questions: "Q2",
+        values: [
+          { option: "A", value: 10 },
+          { option: "B", value: 8 },
+          { option: "C", value: 5 },
+          { option: "D", value: 4 },
+        ],
+      },
+      {
+        questions: "Q4",
+        values: [
+          { option: "A", value: 9 },
+          { option: "B", value: 8 },
+          { option: "C", value: 6 },
+          { option: "D", value: 7 },
+        ],
+      },
+      {
+        questions: "Q5",
+        values: [
+          { option: "A", value: 8 },
+          { option: "B", value: 6 },
+          { option: "C", value: 9 },
+          { option: "D", value: 7 },
+        ],
+      },
+      {
+        questions: "Q7",
+        values: [
+          { option: "A", value: 7 },
+          { option: "B", value: 8 },
+          { option: "C", value: 9 },
+          { option: "D", value: 10 },
+        ],
+      },
+      {
+        questions: "Q8",
+        values: [
+          { option: "A", value: 8 },
+          { option: "B", value: 6 },
+          { option: "C", value: 7 },
+          { option: "D", value: 5 },
+        ],
+      },
+      {
+        questions: "Q10",
+        values: [
+          { option: "A", value: 9 },
+          { option: "B", value: 10 },
+          { option: "C", value: 8 },
+          { option: "D", value: 7 },
+        ],
+      },
     ],
   },
   {
     destination: "Whistler, Canada",
     fields: [
-      { option: "A", value: 3 },
-      { option: "B", value: 10 },
-      { option: "C", value: 2 },
-      { option: "D", value: 6 },
+      {
+        questions: "Q1",
+        values: [
+          { option: "A", value: 3 },
+          { option: "B", value: 10 },
+          { option: "C", value: 2 },
+          { option: "D", value: 6 },
+        ],
+      },
+      {
+        questions: "Q2",
+        values: [
+          { option: "A", value: 2 },
+          { option: "B", value: 8 },
+          { option: "C", value: 6 },
+          { option: "D", value: 10 },
+        ],
+      },
+      {
+        questions: "Q4",
+        values: [
+          { option: "A", value: 6 },
+          { option: "B", value: 8 },
+          { option: "C", value: 9 },
+          { option: "D", value: 7 },
+        ],
+      },
+      {
+        questions: "Q5",
+        values: [
+          { option: "A", value: 7 },
+          { option: "B", value: 10 },
+          { option: "C", value: 8 },
+          { option: "D", value: 9 },
+        ],
+      },
+      {
+        questions: "Q7",
+        values: [
+          { option: "A", value: 3 },
+          { option: "B", value: 8 },
+          { option: "C", value: 10 },
+          { option: "D", value: 9 },
+        ],
+      },
+      {
+        questions: "Q8",
+        values: [
+          { option: "A", value: 6 },
+          { option: "B", value: 8 },
+          { option: "C", value: 9 },
+          { option: "D", value: 7 },
+        ],
+      },
+      {
+        questions: "Q10",
+        values: [
+          { option: "A", value: 7 },
+          { option: "B", value: 8 },
+          { option: "C", value: 10 },
+          { option: "D", value: 9 },
+        ],
+      },
     ],
   },
 ];
@@ -85,11 +784,11 @@ const options = [
 let userChoice = [];
 let points = [];
 
-let numQuestions = 10;
+let numQuestions = 7;
 
 let url = window.location.href;
 let params = new URLSearchParams(window.location.search);
-let weightScore = [2, 1.5, 2, 1.5, 1, 1, 1, 1, 1.5, 2];
+let weightScore = [2, 1.5, 1.5, 1, 1, 1, 2];
 
 const btnCalcDest = document.querySelector('[ap-quiz="calc_dest"]');
 
@@ -99,9 +798,11 @@ function calculateValuePoint() {
       let i = 0;
       let sum = 0;
       dest.fields.forEach((field) => {
-        if (field.option.toUpperCase() === choice) {
-          sum += field.value * weightScore[i];
-        }
+        field.values.forEach((val) => {
+          if (val.option.toUpperCase() === choice) {
+            sum += val.value * weightScore[i];
+          }
+        });
         i++;
       });
       if (!points[dest.destination]) {
@@ -130,7 +831,10 @@ buttonChoiceQuestion.forEach((btnChoice) => {
   btnChoice.addEventListener("click", () => {
     const choice = btnChoice.getAttribute("ap-quiz-choice");
     const parentBtn = btnChoice.parentElement.parentElement;
-    const numberQuestion = parentBtn.querySelector('[ap-quiz-question]').getAttribute("ap-quiz-question");
+    const numberQuestion = parentBtn
+      .querySelector("[ap-quiz-question]")
+      .getAttribute("ap-quiz-question");
+    console.log(choice, parentBtn, numberQuestion);
     params.set(`q${numberQuestion}`, choice);
     url = url.split("?")[0] + "?" + params.toString();
     console.log(url);
